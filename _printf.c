c0e506e81deac287f6e30c4c4531b12d45a72204
shaco-star <sherifm846@gmail.com>
2023-03-27 11:56:08 -0700

    commit

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _printf - printf format
+ *
+ * @format: format
+ *
+ * Return: int
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	char c;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
+					count++;
+					break;
+				case 's':
+					count += write(1, va_arg(args, char*), strlen(va_arg(args, char*)));
+					break;
+				case '%':
+					write(1, '%', 1);
+					count++;
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _PRINTF
+#define _PRINTF
+
+#include <stdio.h>
+#include <stdrag.h>
+
+int _printf(const char *format, ...);
+
+#endif
