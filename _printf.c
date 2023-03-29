39e1c14a52929b38c58170e456295273766f985b
shaco-star <sherifm846@gmail.com>
2023-03-28 17:49:45 -0700

    commit

--- _printf.c
+++ _printf.c
@@ -18,6 +18,10 @@ int _printf(const char *format, ...)
 	char c, *s;
 
 	va_start(args, format);
+	  if (!format || (format[0] == '%' && !format[1]))
+		                      return (-1);
+          if (format[0] == '%' && format[1] == ' ' && !format[2])
+			                          return (-1);
 	for (; *format != '\0'; format++)
 	{
 		if (*format != '%')
