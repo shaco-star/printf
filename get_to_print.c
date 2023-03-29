ad4cb3c30765e342b950477b0b7780be780aa3f6
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-03-29 04:29:32 +0200

    add format ptint

---
+++ get_to_print.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_print - function to selects right printing
+ * 
+ * @s: character
+ * 
+ * Description: structs array
+ * 
+ * Return: pointer
+ */
+
+int (*get_print(char s))(va_list, flags_x *)
+{
+	printh func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal}
+		};
+	int flags = 14;
+
+	register int i;
+
+	for (i = 0; i < flags; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
+}
