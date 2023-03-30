3c27773c6604a377a0136033afadc0427fe7621e
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-03-30 04:28:45 +0200

    f

--- g_flags.c
+++ g_flags.c
@@ -1,35 +1,36 @@
 
+
 #include "main.h"
 
 /**
- * get_flags - Calculates
- * @format: Formatted string
- * @i: parameter.
- * Return: Flags:
+ * get_flag - function to flags
+ *
+ * @s: character
+ * @f: pointer to struct
+ *
+ * Return: 1
  */
-int get_flags(const char *format, int *i)
+
+int get_flag(char s, flags_x *f)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, cr_i;
-	int flags = 0;
-	const char flags_char[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int arr_flags[] = {1, 2, 4, 8, 16, 0};
+	int i = 0;
 
-	for (cr_i = *i + 1; format[cr_i] != '\0'; cr_i++)
+	switch (s)
 	{
-		for (j = 0; flags_char[j] != '\0'; j++)
-			if (format[cr_i] == flags_char[j])
-			{
-				flags |= arr_flags[j];
-				break;
-			}
-
-		if (flags_char[j] == 0)
+		case '+':
+			f->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			f->space = 1;
+			i = 1;
+			break;
+		case '#':
+			f->hash = 1;
+			i = 1;
 			break;
 	}
 
-	*i = cr_i - 1;
-
-	return (flags);
+	return (i);
 }
+
