b3d37cdf0b8e5cc16cbaf127c3eb038da0ba191b
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-04-05 16:43:24 +0200

    f

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * _printf - printf format
@@ -11,26 +10,46 @@
  * Return: int
 */
 
-
 int _printf(const char *format, ...)
 {
-	  specifier_t specifiers[] = {
-		   {"c", print_char},
-		   {"s", print_string},
-		   {"%", print_percent},
-		   {"d", print_digit},
-		   {"i", print_},
-		   {NULL, NULL}
-	   };
+	int (*printfunc)(va_list, flags_x *);
+	const char *p;
 	va_list args;
-	int count = 0;
+	flags_x f = {0, 0, 0};
+
+	register int count = 0;
 
 	va_start(args, format);
 
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
 
-	count = func(format, specifiers, args);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &f))
+				p++;
+			printfunc = get_print(*p);
+			count += (printfunc)
+				? printfunc(args, &f)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
 
 	va_end(args);
 	return (count);
+
 }
+
---
+++ _puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints string
+ * @str: pointer to the string
+ * Return: number
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
+
