cb66d0f9bb78c330b78f84144f07234929c18316
shaco-star <sherifm846@gmail.com>
2023-04-05 07:17:04 -0700

    commit

---
+++ print_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_digit - handle %d %i modifiers
+ *
+ * @args: argument
+ *
+ * Return: int
+*/
+
+int print_digit(va_list args)
+{
+	:wq
+
+}
+
+/**
+ * print_int - handle int
+ *
+ * @n: number
+ *
+ * Return: number
+*/
+
